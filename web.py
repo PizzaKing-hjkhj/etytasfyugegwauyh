22680f32520dd3330067e3207b5c64367909da6d
PizzaKing-hjkhj <132848387+PizzaKing-hjkhj@users.noreply.github.com>
2023-05-07 16:14:43 -0400

    Add files via upload

---
+++ web.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(page_title="idk", page_icon=":pizza:", layout="wide")
+
+st.subheader("Hi :wave: I hope you like this this was my first test")
+st.title("A bunche of stuff")
+st.header("A little about me")
+st.write("""
+My name is Nick
+I love pizza
+I have a brother
+I try my best 
+I do well in school
+I like to leran new things
+Im trying to leran code and im getting better at it all the time""")
+st.write("---")
+left_column, right_column = st.columns(2)
+st.header("what do I do")
+st.write("I do school and play games and stuff like that.")
+st.write("you need to eat this :pizza:https://www.hungryhowies.com It is the best pizza I have ever had")
